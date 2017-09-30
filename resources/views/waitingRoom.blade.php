3fcc899ce5d5769d24a5d3671410f5956e54d812
Daniel Capacio <danielcapacio@gmail.com>
2017-09-30 15:24:27 -0700

    Initial waiting room view added

---
+++ resources/views/waitingRoom.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container" id="room">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-3">
+                                    
+                                </div>
+                                <div class="col-3">
+
+                                </div>
+                                <div class="col-3">
+
+                                </div>
+                                <div class="col-3">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                            {{ session('status') }}
+                            </div>
+                        @endif
+                        <div class="waitingRoom-Code text-center">
+                            <span class="waitingRoom-Label">
+                                Room:
+                                <!-- room variable can go here -->
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            </span>
+                        </div>
+                        <div class="text-center">
+                            <p>Go to upyourstudygame.tech</p>
+                            <p>
+                                Enter your name and the room code
+                                <span class="waitingRoom-Label">
+                                    test
+                                </span>
+                                &nbsp;to join the game
+                            </p>
+                            <!-- logic for host or not -->
+                            <button class="btn btn-lg btn-success">Start</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
