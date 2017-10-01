60af7d3cde57592fba7682fb818690fe0108d0b9
Daniel Capacio <danielcapacio@gmail.com>
2017-10-01 01:22:11 -0700

    Initial mobile view for answer selection page

--- resources/views/secondary/answerSelection.blade.php
+++ resources/views/secondary/answerSelection.blade.php
@@ -10,14 +10,14 @@
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-5 col-md-offset-3" style="{{ 'text-align:center;' }}">
                 <!-- Answers go here -->
-                <div class="panel panel-default">
+                <div class="panel panel-default" style={{ 'text-align:center;' }}>
                     <div class="panel-body" style={{ 'text-align:center;' }}>
-                        <!-- 2 questions per row -->
+                        <!-- 1 question per row -->
                         <div class="container" style={{ 'display:inline-block;' }}>
                             <div class="row">
-                                <div class="col-md-4 col-md-offset-1 text-center">
+                                <div class="col-md-4 text-center">
                                     <a href="#" id="option1">
                                         <div class="panel panel-default">
                                             <div class="panel-body optionAnswer1">
@@ -27,6 +27,8 @@
                                         </div>
                                     </a>
                                 </div>
+                            </div>
+                            <div class="row">
                                 <div class="col-md-4 text-center">
                                     <a href="#" id="option2">
                                         <div class="panel panel-default">
@@ -39,7 +41,7 @@
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-md-4 col-md-offset-1 text-center">
+                                <div class="col-md-4 text-center">
                                     <a href="#" id="option3">
                                         <div class="panel panel-default">
                                             <div class="panel-body optionAnswer3">
@@ -49,6 +51,8 @@
                                         </div>
                                     </a>
                                 </div>
+                            </div>
+                            <div class="row">
                                 <div class="col-md-4 text-center">
                                     <a href="#" id="option4">
                                         <div class="panel panel-default">
@@ -64,17 +68,20 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-md-4 col-md-offset-4">
-            <!-- timer -->
-            <div class="panel panel-default">
-                <div class="panel-body text-center">
-                    <span class="glyphicon glyphicon-time timer"></span>
-                    &nbsp;
-                    <span class="answerSelection-TimeLeft">
-                            5
-                        </span>
-                    &nbsp;seconds left
+            <div class="col-md-5 col-md-offset-3">
+                <!-- timer -->
+                <div class="panel panel-default">
+                    <div class="panel-body text-center">
+                        <p>
+                            <span class="glyphicon glyphicon-time timer"></span>
+                            &nbsp;
+                            <span class="answerSelection-TimeLeft">
+                                5
+                            </span>
+                            &nbsp;seconds left
+                        </p>
+
+                    </div>
                 </div>
             </div>
         </div>
