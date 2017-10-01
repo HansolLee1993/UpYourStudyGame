640bd79f5bb3016ebef1c793593bce9e6245c50a
Daniel Capacio <danielcapacio@gmail.com>
2017-10-01 00:50:55 -0700

    Trying to center answer selections

--- resources/views/primary/answerSelection.blade.php
+++ resources/views/primary/answerSelection.blade.php
@@ -8,16 +8,16 @@
         </div>
         <hr>
     </div>
-    <div class="container" id="answerSelection">
+    <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <!-- Answers go here -->
                 <div class="panel panel-default">
-                    <div class="panel-body text-center">
+                    <div class="panel-body" style={{ 'text-align:center;' }}>
                         <!-- 2 questions per row -->
-                        <div class="container">
+                        <div class="container" style={{ 'display:inline-block;' }}>
                             <div class="row">
-                                <div class="col-md-4">
+                                <div class="col-md-6 text-center">
                                     <a href="#" id="option1">
                                         <div class="panel panel-default">
                                             <div class="panel-body optionAnswer1">
@@ -27,7 +27,7 @@
                                         </div>
                                     </a>
                                 </div>
-                                <div class="col-sm-4 text-center">
+                                <div class="col-md-6 text-center">
                                     <a href="#" id="option2">
                                         <div class="panel panel-default">
                                             <div class="panel-body optionAnswer2">
@@ -39,7 +39,7 @@
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-md-4 text-center">
+                                <div class="col-md-6 text-center">
                                     <a href="#" id="option3">
                                         <div class="panel panel-default">
                                             <div class="panel-body optionAnswer3">
@@ -49,7 +49,7 @@
                                         </div>
                                     </a>
                                 </div>
-                                <div class="col-sm-4 text-center">
+                                <div class="col-sm-6 text-center">
                                     <a href="#" id="option4">
                                         <div class="panel panel-default">
                                             <div class="panel-body optionAnswer4">
@@ -64,17 +64,17 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 col-md-offset-4">
-                <!-- timer -->
-                <div class="panel panel-default">
-                    <div class="panel-body text-center">
-                        <span class="glyphicon glyphicon-time timer"></span>
-                        &nbsp;
-                        <span class="answerSelection-TimeLeft">
+        </div>
+        <div class="col-md-4 col-md-offset-4">
+            <!-- timer -->
+            <div class="panel panel-default">
+                <div class="panel-body text-center">
+                    <span class="glyphicon glyphicon-time timer"></span>
+                    &nbsp;
+                    <span class="answerSelection-TimeLeft">
                             5
                         </span>
-                        &nbsp;seconds left
-                    </div>
+                    &nbsp;seconds left
                 </div>
             </div>
         </div>
